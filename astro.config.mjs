ab0fe94a2cf2e964283396728f1280bf6bec451a
Zag <za12ew44zz_999@hotmail.com>
2026-03-16 21:46:43 +0700

    force update: clean all index

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-
   site: 'https://Zag5A6167.github.io',
-  
-  base: '/', 
+  base: '/',
+  outDir: './dist', 
 });
